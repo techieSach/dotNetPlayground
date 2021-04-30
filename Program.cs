acdab940a2b7ba8b1ee7905dcf162655e7d647b7
techieSach <68861075+techieSach@users.noreply.github.com>
2021-04-30 22:10:22 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestBed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>(){1,2,4,5,6,7,8,9,10,11};
+            // List<int> numbers = new List<int>();
+            Chunk<int> chunker = new Chunk<int>(numbers, 0);
+
+            while (chunker.GetNextChunk())
+            {
+                var chunkedResults  = chunker.CurrentChunk();
+            }
+
+            Console.WriteLine("Done!");
+        }
+    }
+}
